2a6661aa06d85dfb053908f2410cd1f6d9a04abb
Max Xu <maxs.xu@gmail.com>
2022-08-16 16:26:52 +0800

    Remove unused legacy docbot
    Signed-off-by: Max Xu <maxs.xu@gmail.com>

--- doc-label-check/src/patterns.ts
+++
@@ -1,5 +0,0 @@
-// Unescape escaped characters when using these patterns in a YAML file
-export const BACKTICK = '- \\[(.*?)\\] ?`(.+?)`'; // matches "- [ ] `abc` ..."
-export const COLON = '- \\[(.*?)\\] ?(.+?):'; // matches "- [ ] abc: ..."
-export const ASTERISK = '- \\[(.*?)\\] ?\\*(.+?)\\*'; // matches "- [ ] *abc* ..."
-export const DOUBLE_ASTERISK = '- \\[(.*?)\\] ?\\*\\*(.+?)\\*\\*'; // matches "- [ ] **abc** ..."
