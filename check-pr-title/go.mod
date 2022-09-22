2e36236327b770fe71d08250bbb754f04b078424
mango <xu.weiKyrie@foxmail.com>
2022-09-22 14:56:25 +0800

    feat: add action of check-pr-title to ensure PR title matches Naming Convention Guide.
    Signed-off-by: mango <xu.weiKyrie@foxmail.com>

---
+++ check-pr-title/go.mod
@@ -0,0 +1,22 @@
+module github.com/apache/pulsar-test-infra/check-pr-title
+
+go 1.18
+
+require (
+	github.com/apache/pulsar-test-infra/docbot v0.0.0-20220914095413-88062a3edeb2
+	github.com/google/go-github/v45 v45.0.0
+	github.com/migueleliasweb/go-github-mock v0.0.10
+	github.com/sethvargo/go-githubactions v1.0.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
+
+require (
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-github/v41 v41.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/sethvargo/go-envconfig v0.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
---
+++ check-pr-title/pkg/action/action_test.go
@@ -0,0 +1,206 @@
+package action
+
+import (
+	"context"
+	"github.com/sethvargo/go-githubactions"
+	"os"
+	"testing"
+
+	"github.com/google/go-github/v45/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+)
+
+func mustNewActionConfig() *Config {
+	_ = os.Setenv("GITHUB_REPOSITORY", "apache/pulsar")
+	_ = os.Setenv("INPUT_TYPES", "feat\n            improve\n            fix\n            cleanup\n            refactor\n            revert")
+	_ = os.Setenv("INPUT_SCOPES", "admin\n            broker\n            cli\n            io\n            fn\n            meta\n            monitor\n            proxy\n            schema\n            sec\n            sql\n            storage\n            offload\n            txn\n            java\n            cpp\n            py\n            ws\n            test\n            ci\n            build\n            misc\n            doc\n            blog\n            site")
+	_ = os.Setenv("INPUT_HEADERPATTERN", "^(?:\\[(\\w+)\\])?(?:\\[(\\w+)\\])? (.+)$")
+
+	githubAction := githubactions.New()
+	config, err := NewActionConfig(githubAction)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
+
+func mustNewActionWithDefaultConfig() *Config {
+	_ = os.Setenv("GITHUB_REPOSITORY", "apache/pulsar")
+
+	githubAction := githubactions.New()
+	config, err := NewActionConfig(githubAction)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
+
+func assertMessageLabel(t *testing.T, err error, message string) {
+	t.Helper()
+
+	if err == nil {
+		t.Fatal("Expect err not nil")
+	}
+
+	if err.Error() != message {
+		t.Fatal("Expect err equals " + message)
+	}
+}
+
+func TestTitleMatched(t *testing.T) {
+	id := int64(1)
+	title := "[feat][ci] Support to check pr title"
+
+	mockedHTTPClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatch(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			github.PullRequest{
+				ID:     &id,
+				Title:  &title,
+				Labels: nil,
+			},
+		),
+	)
+
+	config := mustNewActionConfig()
+	action := NewActionWithClient(context.Background(), config, github.NewClient(mockedHTTPClient))
+
+	err := action.Run(int(id), openedActionType)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestTypeOfTitleUnMatched(t *testing.T) {
+	id := int64(1)
+	title := "[rewrite][ci] Support to check pr title"
+
+	userLogin := "mango"
+	mockedHTTPClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatch(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			github.PullRequest{
+				ID: &id,
+				User: &github.User{
+					Login: &userLogin,
+				},
+				Title:  &title,
+				Labels: nil,
+			},
+		),
+		mock.WithRequestMatch(mock.PostReposIssuesCommentsByOwnerByRepoByIssueNumber, nil),
+	)
+
+	config := mustNewActionConfig()
+	action := NewActionWithClient(context.Background(), config, github.NewClient(mockedHTTPClient))
+
+	err := action.Run(int(id), openedActionType)
+	assertMessageLabel(t, err, TitleUnmatchPattern)
+}
+
+func TestScopeOfTitleUnMatched(t *testing.T) {
+	id := int64(1)
+	title := "[feat][ci1] Support to check pr title"
+
+	userLogin := "mango"
+	mockedHTTPClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatch(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			github.PullRequest{
+				ID: &id,
+				User: &github.User{
+					Login: &userLogin,
+				},
+				Title:  &title,
+				Labels: nil,
+			},
+		),
+		mock.WithRequestMatch(mock.PostReposIssuesCommentsByOwnerByRepoByIssueNumber, nil),
+	)
+
+	config := mustNewActionConfig()
+	action := NewActionWithClient(context.Background(), config, github.NewClient(mockedHTTPClient))
+
+	err := action.Run(int(id), openedActionType)
+	assertMessageLabel(t, err, TitleUnmatchPattern)
+}
+
+func TestEmptyTypeOfTitleUnMatched(t *testing.T) {
+	id := int64(1)
+	title := "[][ci1] Support to check pr title"
+
+	userLogin := "mango"
+	mockedHTTPClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatch(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			github.PullRequest{
+				ID: &id,
+				User: &github.User{
+					Login: &userLogin,
+				},
+				Title:  &title,
+				Labels: nil,
+			},
+		),
+		mock.WithRequestMatch(mock.PostReposIssuesCommentsByOwnerByRepoByIssueNumber, nil),
+	)
+
+	config := mustNewActionConfig()
+	action := NewActionWithClient(context.Background(), config, github.NewClient(mockedHTTPClient))
+
+	err := action.Run(int(id), openedActionType)
+	assertMessageLabel(t, err, TitleUnmatchPattern)
+}
+
+func TestEmptyScopeOfTitleUnMatched(t *testing.T) {
+	id := int64(1)
+	title := "[feat][] Support to check pr title"
+
+	userLogin := "mango"
+	mockedHTTPClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatch(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			github.PullRequest{
+				ID: &id,
+				User: &github.User{
+					Login: &userLogin,
+				},
+				Title:  &title,
+				Labels: nil,
+			},
+		),
+		mock.WithRequestMatch(mock.PostReposIssuesCommentsByOwnerByRepoByIssueNumber, nil),
+	)
+
+	config := mustNewActionConfig()
+	action := NewActionWithClient(context.Background(), config, github.NewClient(mockedHTTPClient))
+
+	err := action.Run(int(id), openedActionType)
+	assertMessageLabel(t, err, TitleUnmatchPattern)
+}
+
+func TestTitleMatchedWithDefaultConfig(t *testing.T) {
+	id := int64(1)
+	title := "[feat][ci] Support to check pr title"
+
+	mockedHTTPClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatch(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			github.PullRequest{
+				ID:     &id,
+				Title:  &title,
+				Labels: nil,
+			},
+		),
+	)
+
+	config := mustNewActionWithDefaultConfig()
+	action := NewActionWithClient(context.Background(), config, github.NewClient(mockedHTTPClient))
+
+	err := action.Run(int(id), openedActionType)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
